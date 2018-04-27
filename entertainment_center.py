8d16217b2b1e6655edef813a699262b0a601b8bc
Jay Shah <reversearrow@gmail.com>
2018-04-27 10:39:13 -0400

    Fix:

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,17 +1,17 @@
 from fresh_tomatoes import open_movies_page
 from media import Movies
 
-# Creating a Movie Object called Dark Knight.# NOQA
-dark_knight = Movies("Dark Knight", "https://ca.movieposter.com/posters/archive/main/69/MPW-34753",  # NOQA
+# Creating a Movie Object called Dark Knight.
+dark_knight = Movies("Dark Knight", "https://ca.movieposter.com/posters/archive/main/69/MPW-34753",
                      "https://www.youtube.com/watch?v=EXeTwQWrcwY")
 
 # Creating a Movie Object called avengers.
-avengers = Movies("Avengers", "https://ca.movieposter.com/posters/archive/main/152/MPW-76269",  # NOQA
+avengers = Movies("Avengers", "https://ca.movieposter.com/posters/archive/main/152/MPW-76269",
                   "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
 
 # Creating a Movie object called matrix.
-matrix = Movies("Matrix", "https://ca.movieposter.com/posters/archive/main/9/A70-4902",  # NOQA
+matrix = Movies("Matrix", "https://ca.movieposter.com/posters/archive/main/9/A70-4902",
                 "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
 # Creating a list, adding movie objectes inside it.
